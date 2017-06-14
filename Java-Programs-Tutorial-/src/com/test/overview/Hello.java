c781298fd83346301e29d0a6377361fa59a980cd
Sravani Muraonda <murakondasravani64@gmail.com>
2017-06-14 17:55:27 -0500

    Storing Overview chapter notes and programs

--- Java-Programs-Tutorial-/src/com/test/overview/Hello.java
+++ Java-Programs-Tutorial-/src/com/test/overview/Hello.java
@@ -1,12 +1,8 @@
-
+package com.test.overview;
 public class Hello {
 
-	public Hello() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.print("Hello World");
 
 	}
 
